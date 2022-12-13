37591b39b85367c3e9c59af07810777008580609
robert-vm <gainarobert47@gmail.com>
2022-12-13 22:12:49 +0100

    Finished exercise 2.5

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -1,3 +1,5 @@
+import Course from './components/Course';
+
 const App = () => {
   const courses = [
     {
@@ -51,52 +53,6 @@ const App = () => {
   )
 }
 
-const Course = ({course}) => {
-  return (
-    <div>
-      <Header name={course.name}/>
-      <Content course={course}/>
-    </div>
-  )
-}
-
-const Header = ({name}) => {
-  return (
-    <>
-    <h1>{name}</h1>
-    </>
-
-  );
-}
-
-const Content = ({course}) => {
-  return(
-    <div>
-      {course.parts.map(element =>
-        <Part key={element.id} part={element.name} exercises={element.exercises}/>
-      )}
-      <Total parts={course.parts}/>
-    </div>
-  );
-}
-
-const Total = ({parts}) => {
-  let exercises = parts.map(element => element.exercises);
-  let sum = exercises.reduce((ac, cv) => ac + cv);
-  return (
-    <div>
-      <h4>total of {sum} exercises</h4>
-    </div>
-  )
-}
-
-const Part = (props) => {
-  return (
-    <>
-      <p>{props.part} {props.exercises}</p>
-    </>
-  );
-}
 
 
 export default App;
