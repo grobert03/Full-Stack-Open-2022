edbd02b1b7d9f062201a6c00bd4673bbd8ee2189
grobert03 <gainarobert47@gmail.com>
2023-05-27 20:30:40 +0200

    Finished exercise 2.12

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,92 +1,112 @@
 import { useState, useEffect } from "react";
-import axios from 'axios';
+import personService from "./services/persons";
 const App = () => {
   const [persons, setPersons] = useState([]);
-  const [newName, setNewName] = useState('');
-  const [newNumber, setNewNumber] = useState('');
-  const [filter, setFilter] = useState('');
-
+  const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
+  const [filter, setFilter] = useState("");
 
   useEffect(() => {
-    axios.get('http://localhost:3001/persons').then(
-      response => {
-        setPersons(response.data);
-      }
-    )
+    personService.getAll().then((initialNotes) => setPersons(initialNotes));
   }, []);
 
   const addPerson = (event) => {
     event.preventDefault();
 
     // Check if name already exists
-    if (persons.filter(person => JSON.stringify(person.name) === JSON.stringify(newName)).length === 0) {
-      setPersons(persons.concat({name: newName, number: newNumber, id: persons.length + 1}));
+    if (
+      persons.filter(
+        (person) => JSON.stringify(person.name) === JSON.stringify(newName)
+      ).length === 0
+    ) {
+      let newObject = {
+        name: newName,
+        number: newNumber,
+        id: persons.length + 1,
+      };
+      personService.create(newObject).then((newObject) => {
+        setPersons(persons.concat(newObject));
+      });
     } else {
       alert(`${newName} is already added to phonebook`);
     }
-  }
+  };
 
   const handleNameChange = (event) => {
     setNewName(event.target.value);
-  }
+  };
 
   const handleNumberChange = (event) => {
     setNewNumber(event.target.value);
-  }
+  };
 
   const handleFilter = (event) => {
     setFilter(event.target.value.toLowerCase());
-  }
+  };
 
-  const personsToShow = filter === '' ? persons : persons.filter(element => element.name.toLowerCase().includes(filter));
+  const personsToShow =
+    filter === ""
+      ? persons
+      : persons.filter((element) =>
+          element.name.toLowerCase().includes(filter)
+        );
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <Filter changeHandler={handleFilter}/>
+      <Filter changeHandler={handleFilter} />
       <h2>add a new</h2>
-      <PersonForm submitHandler={addPerson} nameHandler={handleNameChange} numberHandler={handleNumberChange}/>
+      <PersonForm
+        submitHandler={addPerson}
+        nameHandler={handleNameChange}
+        numberHandler={handleNumberChange}
+      />
       <h2>Numbers</h2>
-      <Persons toShow={personsToShow}/>
+      <Persons toShow={personsToShow} />
     </div>
-  )
-}
+  );
+};
 
-const Filter = ({changeHandler}) => {
+const Filter = ({ changeHandler }) => {
   return (
     <div>
-    filter shown with <input onChange={changeHandler}/>
+      filter shown with <input onChange={changeHandler} />
     </div>
-  )
-}
+  );
+};
 
-const PersonForm = ({submitHandler, nameHandler, numberHandler}) => {
+const PersonForm = ({ submitHandler, nameHandler, numberHandler }) => {
   return (
     <form onSubmit={submitHandler}>
-        <div>
-          name: <input onChange={nameHandler}/>
-        </div>
-        <div>
-          number: <input onChange={numberHandler}/>
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
+      <div>
+        name: <input onChange={nameHandler} />
+      </div>
+      <div>
+        number: <input onChange={numberHandler} />
+      </div>
+      <div>
+        <button type="submit">add</button>
+      </div>
     </form>
-  )
-}
+  );
+};
 
-const Persons = ({toShow}) => {
+const Persons = ({ toShow }) => {
   return (
     <div>
-      {toShow.map(element => <Person key={element.id} name={element.name} number={element.number}/>)}
+      {toShow.map((element) => (
+        <Person key={element.id} name={element.name} number={element.number} />
+      ))}
     </div>
-  )
-}
+  );
+};
 
-const Person = ({name, number}) => {
-  return <li>{name} {number}</li>
-}
-
+const Person = ({ name, number }) => {
+  return (
+    <li>
+      {name} {number}
+    </li>
+  );
+};
 
 export default App;
