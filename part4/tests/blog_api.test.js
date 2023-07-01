985b326bd2d3c70380b5246d61d1ffbaeb25cafe
grobert03 <gainarobert47@gmail.com>
2023-07-01 07:30:13 +0200

    Finished exercise 4.10

--- part4/tests/blog_api.test.js
+++ part4/tests/blog_api.test.js
@@ -32,16 +32,30 @@ beforeEach(async () => {
 });
 
 test("returns all of the posts (4.8)", async () => {
-    const response = await api.get('/api/blogs');
-    console.log(response.body);
-    expect(response.body).toHaveLength(2);
-})
+  const response = await api.get("/api/blogs");
+  expect(response.body).toHaveLength(2);
+});
 
 test("identifier is named id (4.9)", async () => {
-    const response = await api.get('/api/blogs');
-    response.body.forEach(r => {
-        expect(r.id).toBeDefined();
-    })
+  const response = await api.get("/api/blogs");
+  response.body.forEach((r) => {
+    expect(r.id).toBeDefined();
+  });
+});
+
+test("blog post is added (4.10)", async () => {
+  const newPost = {
+    title: "Test3",
+    author: "Robert",
+    url: "test3",
+    likes: 1,
+  };
+  const response = await api.post("/api/blogs").send(newPost);
+  
+  const blogs = await api.get("/api/blogs");
+
+  expect(blogs.body).toHaveLength(3);
+  expect(response.body.title).toEqual("Test3");
 });
 
 afterAll(async () => {
