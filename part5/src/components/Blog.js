895b5bafec8d5f8d59c53eae67e9ab412ee4ce3d
grobert03 <gainarobert47@gmail.com>
2023-07-11 10:49:16 +0200

    Finished exercise 5.7

--- part5/src/components/Blog.js
+++ part5/src/components/Blog.js
@@ -1,5 +1,44 @@
-const Blog = ({blog}) => {
-  return <div>{blog.title} {blog.author}</div>
-}
+import { useState } from "react";
+
+const Blog = ({ blog }) => {
+  let [visible, setVisibility] = useState(false);
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+  const showDetails = () => {
+    return (<div>
+      <div>
+        {blog.likes}<button>like</button>
+      </div>
+      <div>
+        {blog.url}
+      </div>
+      <div>
+        {blog.author}
+      </div>
+    </div>);
+  }
+  return (
+    <div style={blogStyle}>
+      <div>
+        {blog.title} {blog.author}{" "}
+        <button
+          onClick={() => {
+            setVisibility(!visible);
+          }}
+        >
+          {visible ? "hide" : "view"}
+        </button>
+      </div>
+      <div>
+        {visible && showDetails()}
+      </div>
+    </div>
+  );
+};
 
 export default Blog;
